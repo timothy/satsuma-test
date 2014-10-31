b6497d05140a5702f4bb2c3c59f78e8151c25f2d
tylerharding <tharding732@gmail.com>
2014-10-31 13:12:30 -0700

    TestHighNumberOfNodes

--- Satsuma.Test/tests/ReverseGraphTest.cs
+++ Satsuma.Test/tests/ReverseGraphTest.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿//By Ty Harding
+
+
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Satsuma.Test.tests
@@ -12,8 +15,36 @@ namespace Satsuma.Test.tests
             var c = new CompleteGraph(5,Directedness.Directed);
             var g = new ReverseGraph(c);
 
-            Assert.AreEqual(g.NodeCount(), 5);
+            Assert.AreEqual(g.NodeCount(), 5); //test that a graph with 5 nodes is created
         }
+
+        [TestMethod]
+        public void TestZeroNumNodes()
+        {
+            var c = new CompleteGraph(0, Directedness.Directed);
+            var g = new ReverseGraph(c);
+
+            Assert.AreEqual(g.NodeCount(), 0); //test that a graph with 0 nodes is created
+        }
+
+        [TestMethod]
+        public void TestNegativeNumNodes()
+        {
+            var c = new CompleteGraph(-1, Directedness.Directed);
+            var g = new ReverseGraph(c);
+
+            Assert.AreEqual(g.NodeCount(), -1); //what happens when a graph with negative nodes is created?
+        }
+
+        [TestMethod]
+        public void TestHighNumberOfNodes()
+        {
+            var c = new CompleteGraph(1000, Directedness.Directed);
+            var g = new ReverseGraph(c);
+
+            Assert.AreEqual(g.NodeCount(), 1000);
+        }
+
 
     }
 }
