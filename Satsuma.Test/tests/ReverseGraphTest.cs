21caf9a2e4efdd76d49a6c2d87bc6124b8d88dcc
tylerharding <tharding732@gmail.com>
2014-10-31 13:07:47 -0700

    Created tests for creating a graph with specific number of nodes

---
+++ Satsuma.Test/tests/ReverseGraphTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Satsuma.Test.tests
+{
+    [TestClass]
+    public class ReverseGraphTest
+    {
+        [TestMethod]
+        public void TestCreateReverse()
+        {
+            var c = new CompleteGraph(5,Directedness.Directed);
+            var g = new ReverseGraph(c);
+
+            Assert.AreEqual(g.NodeCount(), 5);
+        }
+
+    }
+}
