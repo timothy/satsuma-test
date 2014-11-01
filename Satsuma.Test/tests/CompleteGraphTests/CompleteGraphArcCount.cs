3a8e25f341e34190591f3e805ec39548530453d0
Matthew Draper <draper_matthewjohn@yahoo.com>
2014-11-01 15:46:13 -0700

    Add tests to CompleteGraph Functions (sorry for the large commit, should have commited more often)

---
+++ Satsuma.Test/tests/CompleteGraphTests/CompleteGraphArcCount.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace Satsuma.Test.tests.CompleteGraphTests
+{
+	[TestClass]
+	public class CompleteGraphArcCount
+	{
+		#region arcs1
+		[TestMethod]
+		public void UndirectedAll()
+		{
+			var g = new CompleteGraph(5, Directedness.Undirected);
+
+			int arcs = g.ArcCount(ArcFilter.All);
+
+			Assert.AreEqual(10, arcs);
+		}
+
+		[TestMethod]
+		public void UndirectedEdge()
+		{
+			var g = new CompleteGraph(5, Directedness.Undirected);
+
+			int arcs = g.ArcCount(ArcFilter.Edge);
+
+			Assert.AreEqual(10, arcs);
+		}
+
+		[TestMethod]
+		public void DirectedAll()
+		{
+			var g = new CompleteGraph(5, Directedness.Directed);
+
+			int arcs = g.ArcCount(ArcFilter.All);
+
+			Assert.AreEqual(20, arcs);
+		}
+
+		[TestMethod]
+		public void DirectedEdge()
+		{
+			var g = new CompleteGraph(5, Directedness.Directed);
+
+			int arcs = g.ArcCount(ArcFilter.Edge);
+
+			Assert.AreEqual(0, arcs);
+		}
+
+		[TestMethod]
+		public void ArcsInvariant()
+		{
+			var g = new CompleteGraph(5, Directedness.Undirected);
+
+			int arcs1 = g.ArcCount(ArcFilter.All);
+			int arcs2 = g.ArcCount(ArcFilter.All);
+
+			Assert.AreEqual(arcs1, arcs2);
+		}
+
+		#endregion
+		#region arcs2
+		[TestMethod]
+		public void FromNodeUndirectedAll()
+		{
+			var g = new CompleteGraph(5, Directedness.Undirected);
+
+			int arcs = g.ArcCount(g.GetNode(0), ArcFilter.All);
+
+			Assert.AreEqual(4, arcs);
+		}
+
+		[TestMethod]
+		public void FromNodeUndirectedEdge()
+		{
+			var g = new CompleteGraph(5, Directedness.Undirected);
+
+			int arcs = g.ArcCount(g.GetNode(0), ArcFilter.Edge);
+
+			Assert.AreEqual(4, arcs);
+		}
+
+		[TestMethod]
+		public void FromNodeUndirectedBackward()
+		{
+			var g = new CompleteGraph(5, Directedness.Undirected);
+
+			int arcs = g.ArcCount(g.GetNode(0), ArcFilter.Backward);
+
+			Assert.AreEqual(4, arcs);
+		}
+
+		[TestMethod]
+		public void FromNodeUndirectedForward()
+		{
+			var g = new CompleteGraph(5, Directedness.Undirected);
+
+			int arcs = g.ArcCount(g.GetNode(0), ArcFilter.Forward);
+
+			Assert.AreEqual(4, arcs);
+		}
+
+		[TestMethod]
+		public void FromNodeDirectedAll()
+		{
+			var g = new CompleteGraph(5, Directedness.Directed);
+
+			int arcs = g.ArcCount(g.GetNode(0), ArcFilter.All);
+
+			Assert.AreEqual(8, arcs);
+		}
+
+		[TestMethod]
+		public void FromNodeDirectedEdge()
+		{
+			var g = new CompleteGraph(5, Directedness.Directed);
+
+			int arcs = g.ArcCount(g.GetNode(0), ArcFilter.Edge);
+
+			Assert.AreEqual(0, arcs);
+		}
+
+		[TestMethod]
+		public void FromNodeDirectedBackward()
+		{
+			var g = new CompleteGraph(5, Directedness.Directed);
+
+			int arcs = g.ArcCount(g.GetNode(0), ArcFilter.Backward);
+
+			Assert.AreEqual(4, arcs);
+		}
+
+		[TestMethod]
+		public void FromNodeDirectedForward()
+		{
+			var g = new CompleteGraph(5, Directedness.Directed);
+
+			int arcs = g.ArcCount(g.GetNode(0), ArcFilter.Forward);
+
+			Assert.AreEqual(4, arcs);
+		}
+
+		[TestMethod]
+		public void FromNodeArcsInvariant()
+		{
+			var g = new CompleteGraph(5, Directedness.Undirected);
+
+			int arcs1 = g.ArcCount(g.GetNode(0), ArcFilter.All);
+			int arcs2 = g.ArcCount(g.GetNode(0), ArcFilter.All);
+
+			Assert.AreEqual(arcs1, arcs2);
+		}
+		#endregion
+		#region arcs3
+		[TestMethod]
+		public void NodeToNodeUndirectedAll()
+		{
+			var g = new CompleteGraph(5, Directedness.Undirected);
+
+			int arcs = g.ArcCount(g.GetNode(0), g.GetNode(1), ArcFilter.All);
+
+			Assert.AreEqual(1, arcs);
+		}
+
+		[TestMethod]
+		public void NodeToNodeUndirectedEdge()
+		{
+			var g = new CompleteGraph(5, Directedness.Undirected);
+
+			int arcs = g.ArcCount(g.GetNode(0), g.GetNode(1), ArcFilter.Edge);
+
+			Assert.AreEqual(1, arcs);
+		}
+
+		[TestMethod]
+		public void NodeToNodeUndirectedBackward()
+		{
+			var g = new CompleteGraph(5, Directedness.Undirected);
+
+			int arcs = g.ArcCount(g.GetNode(0), g.GetNode(1), ArcFilter.Backward);
+
+			Assert.AreEqual(1, arcs);
+		}
+
+		[TestMethod]
+		public void NodeToNodeUndirectedForward()
+		{
+			var g = new CompleteGraph(5, Directedness.Undirected);
+
+			int arcs = g.ArcCount(g.GetNode(0), g.GetNode(1), ArcFilter.Forward);
+
+			Assert.AreEqual(1, arcs);
+		}
+
+		[TestMethod]
+		public void NodeToNodeDirectedAll()
+		{
+			var g = new CompleteGraph(5, Directedness.Directed);
+
+			int arcs = g.ArcCount(g.GetNode(0), g.GetNode(1), ArcFilter.All);
+
+			Assert.AreEqual(2, arcs);
+		}
+
+		[TestMethod]
+		public void NodeToNodeDirectedEdge()
+		{
+			var g = new CompleteGraph(5, Directedness.Directed);
+
+			int arcs = g.ArcCount(g.GetNode(0), g.GetNode(1), ArcFilter.Edge);
+
+			Assert.AreEqual(0, arcs);
+		}
+
+		[TestMethod]
+		public void NodeToNodeDirectedBackward()
+		{
+			var g = new CompleteGraph(5, Directedness.Directed);
+
+			int arcs = g.ArcCount(g.GetNode(0), g.GetNode(1), ArcFilter.Backward);
+
+			Assert.AreEqual(1, arcs);
+		}
+
+		[TestMethod]
+		public void NodeToNodeDirectedForward()
+		{
+			var g = new CompleteGraph(5, Directedness.Directed);
+
+			int arcs = g.ArcCount(g.GetNode(0), g.GetNode(1), ArcFilter.Forward);
+
+			Assert.AreEqual(1, arcs);
+		}
+
+		[TestMethod]
+		public void NodeToNodeArcsInvariant()
+		{
+			var g = new CompleteGraph(5, Directedness.Undirected);
+
+			int arcs1 = g.ArcCount(g.GetNode(0), g.GetNode(1), ArcFilter.All);
+			int arcs2 = g.ArcCount(g.GetNode(0), g.GetNode(1), ArcFilter.All);
+
+			Assert.AreEqual(arcs1, arcs2);
+		}
+		#endregion
+	}
+}
---
+++ Satsuma.Test/tests/CompleteGraphTests/CompleteGraphFiveNodesArcsTests.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace Satsuma.Test.tests.CompleteGraphTests
+{
+	[TestClass]
+	public class CompleteGraphFiveNodesArcsTests
+	{
+		#region arcs1
+		[TestMethod]
+		public void UndirectedAll()
+		{
+			var g = new CompleteGraph(5, Directedness.Undirected);
+
+			var arcs = g.Arcs(ArcFilter.All);
+
+			Assert.AreEqual(10, arcs.Count());
+		}
+
+		[TestMethod]
+		public void UndirectedEdge()
+		{
+			var g = new CompleteGraph(5, Directedness.Undirected);
+
+			var arcs = g.Arcs(ArcFilter.Edge);
+
+			Assert.AreEqual(10, arcs.Count());
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentException))]
+		public void UndirectedBackwardShouldThrow()
+		{
+			var g = new CompleteGraph(5, Directedness.Undirected);
+
+			var arcs = g.Arcs(ArcFilter.Backward);
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentException))]
+		public void UndirectedForwardShouldThrow()
+		{
+			var g = new CompleteGraph(5, Directedness.Undirected);
+
+			var arcs = g.Arcs(ArcFilter.Forward);
+		}
+
+		[TestMethod]
+		public void DirectedAll()
+		{
+			var g = new CompleteGraph(5, Directedness.Directed);
+
+			var arcs = g.Arcs(ArcFilter.All);
+
+			Assert.AreEqual(20, arcs.Count());
+		}
+
+		[TestMethod]
+		public void DirectedEdge()
+		{
+			var g = new CompleteGraph(5, Directedness.Directed);
+
+			var arcs = g.Arcs(ArcFilter.Edge);
+
+			Assert.AreEqual(0, arcs.Count());
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentException))]
+		public void DirectedBackwardShouldThrow()
+		{
+			var g = new CompleteGraph(5, Directedness.Directed);
+
+			var arcs = g.Arcs(ArcFilter.Backward);
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentException))]
+		public void DirectedForwardShouldThrow()
+		{
+			var g = new CompleteGraph(5, Directedness.Directed);
+
+			var arcs = g.Arcs(ArcFilter.Forward);
+		}
+
+		[TestMethod]
+		public void ArcsInvariant()
+		{
+			var g = new CompleteGraph(5, Directedness.Undirected);
+
+			var arcs1 = g.Arcs(ArcFilter.All);
+			var arcs2 = g.Arcs(ArcFilter.All);
+
+			CollectionAssert.AreEqual(arcs1.ToList(), arcs2.ToList());
+		}
+
+		#endregion
+		#region arcs2
+		[TestMethod]
+		public void FromNodeUndirectedAll()
+		{
+			var g = new CompleteGraph(5, Directedness.Undirected);
+
+			var arcs = g.Arcs(g.GetNode(0),ArcFilter.All);
+
+			Assert.AreEqual(4, arcs.Count());
+		}
+
+		[TestMethod]
+		public void FromNodeUndirectedEdge()
+		{
+			var g = new CompleteGraph(5, Directedness.Undirected);
+
+			var arcs = g.Arcs(g.GetNode(0), ArcFilter.Edge);
+
+			Assert.AreEqual(4, arcs.Count());
+		}
+
+		[TestMethod]
+		public void FromNodeUndirectedBackward()
+		{
+			var g = new CompleteGraph(5, Directedness.Undirected);
+
+			var arcs = g.Arcs(g.GetNode(0), ArcFilter.Backward);
+
+			Assert.AreEqual(4, arcs.Count());
+		}
+
+		[TestMethod]
+		public void FromNodeUndirectedForward()
+		{
+			var g = new CompleteGraph(5, Directedness.Undirected);
+
+			var arcs = g.Arcs(g.GetNode(0), ArcFilter.Forward);
+
+			Assert.AreEqual(4, arcs.Count());
+		}
+
+		[TestMethod]
+		public void FromNodeDirectedAll()
+		{
+			var g = new CompleteGraph(5, Directedness.Directed);
+
+			var arcs = g.Arcs(g.GetNode(0), ArcFilter.All);
+
+			Assert.AreEqual(8, arcs.Count());
+		}
+
+		[TestMethod]
+		public void FromNodeDirectedEdge()
+		{
+			var g = new CompleteGraph(5, Directedness.Directed);
+
+			var arcs = g.Arcs(g.GetNode(0), ArcFilter.Edge);
+
+			Assert.AreEqual(0, arcs.Count());
+		}
+
+		[TestMethod]
+		public void FromNodeDirectedBackward()
+		{
+			var g = new CompleteGraph(5, Directedness.Directed);
+
+			var arcs = g.Arcs(g.GetNode(0), ArcFilter.Backward);
+
+			Assert.AreEqual(4, arcs.Count());
+		}
+
+		[TestMethod]
+		public void FromNodeDirectedForward()
+		{
+			var g = new CompleteGraph(5, Directedness.Directed);
+
+			var arcs = g.Arcs(g.GetNode(0), ArcFilter.Forward);
+
+			Assert.AreEqual(4, arcs.Count());
+		}
+
+		[TestMethod]
+		public void FromNodeArcsInvariant()
+		{
+			var g = new CompleteGraph(5, Directedness.Undirected);
+
+			var arcs1 = g.Arcs(g.GetNode(0), ArcFilter.All);
+			var arcs2 = g.Arcs(g.GetNode(0), ArcFilter.All);
+
+			CollectionAssert.AreEqual(arcs1.ToList(), arcs2.ToList());
+		}
+		#endregion
+		#region arcs3
+		[TestMethod]
+		public void NodeToNodeUndirectedAll()
+		{
+			var g = new CompleteGraph(5, Directedness.Undirected);
+
+			var arcs = g.Arcs(g.GetNode(0),g.GetNode(1),ArcFilter.All);
+
+			Assert.AreEqual(1, arcs.Count());
+		}
+
+		[TestMethod]
+		public void NodeToNodeUndirectedEdge()
+		{
+			var g = new CompleteGraph(5, Directedness.Undirected);
+
+			var arcs = g.Arcs(g.GetNode(0),g.GetNode(1),ArcFilter.Edge);
+
+			Assert.AreEqual(1, arcs.Count());
+		}
+
+		[TestMethod]
+		public void NodeToNodeUndirectedBackward()
+		{
+			var g = new CompleteGraph(5, Directedness.Undirected);
+
+			var arcs = g.Arcs(g.GetNode(0),g.GetNode(1),ArcFilter.Backward);
+
+			Assert.AreEqual(1, arcs.Count());
+		}
+
+		[TestMethod]
+		public void NodeToNodeUndirectedForward()
+		{
+			var g = new CompleteGraph(5, Directedness.Undirected);
+
+			var arcs = g.Arcs(g.GetNode(0),g.GetNode(1),ArcFilter.Forward);
+
+			Assert.AreEqual(1, arcs.Count());
+		}
+
+		[TestMethod]
+		public void NodeToNodeDirectedAll()
+		{
+			var g = new CompleteGraph(5, Directedness.Directed);
+
+			var arcs = g.Arcs(g.GetNode(0),g.GetNode(1),ArcFilter.All);
+
+			Assert.AreEqual(2, arcs.Count());
+		}
+
+		[TestMethod]
+		public void NodeToNodeDirectedEdge()
+		{
+			var g = new CompleteGraph(5, Directedness.Directed);
+
+			var arcs = g.Arcs(g.GetNode(0),g.GetNode(1),ArcFilter.Edge);
+
+			Assert.AreEqual(0, arcs.Count());
+		}
+
+		[TestMethod]
+		public void NodeToNodeDirectedBackward()
+		{
+			var g = new CompleteGraph(5, Directedness.Directed);
+
+			var arcs = g.Arcs(g.GetNode(0),g.GetNode(1),ArcFilter.Backward);
+
+			Assert.AreEqual(1, arcs.Count());
+		}
+
+		[TestMethod]
+		public void NodeToNodeDirectedForward()
+		{
+			var g = new CompleteGraph(5, Directedness.Directed);
+
+			var arcs = g.Arcs(g.GetNode(0),g.GetNode(1),ArcFilter.Forward);
+
+			Assert.AreEqual(1, arcs.Count());
+		}
+
+		[TestMethod]
+		public void NodeToNodeArcsInvariant()
+		{
+			var g = new CompleteGraph(5, Directedness.Undirected);
+
+			var arcs1 = g.Arcs(g.GetNode(0),g.GetNode(1),ArcFilter.All);
+			var arcs2 = g.Arcs(g.GetNode(0),g.GetNode(1),ArcFilter.All);
+
+			CollectionAssert.AreEqual(arcs1.ToList(), arcs2.ToList());
+		}
+		#endregion
+	}
+}
