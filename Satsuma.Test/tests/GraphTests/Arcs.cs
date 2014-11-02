efbb014dcf1f2cac5ebe65e93a23dc657ef987e5
WesThompson <thompsonwesley8@yahoo.com>
2014-11-02 15:49:25 -0800

    commit

---
+++ Satsuma.Test/tests/GraphTests/Arcs.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Satsuma.Test.tests.GraphTests
+{
+    [TestClass]
+    public class Arcs
+    {
+        [TestMethod]
+        public void ArcToStringOverride()
+        {
+            var a1 = new Arc(10);
+
+            Assert.AreEqual("|10", a1.ToString());
+        }
+
+        [TestMethod]
+        public void FalseArcID()
+        {
+            var a1 = new Arc(1);
+            var a2 = new Arc(20);
+
+            Assert.IsFalse(a1.Equals(a2));
+        }
+
+        [TestMethod]
+        public void TrueArcID()
+        {
+            var a1 = new Arc(3534);
+            var a2 = new Arc(3534);
+
+            Assert.IsTrue(a1.Equals(a2));
+        }
+
+        [TestMethod]
+        public void NegativeArcID()
+        {
+            var a1 = new Arc(-234);
+
+            Assert.AreEqual(-234, a1.Id);
+        }
+
+        [TestMethod]
+        public void NegativeArcIDToString()
+        {
+            var a1 = new Arc(-353);
+
+            Assert.AreEqual("|-353", a1.ToString());
+        }
+
+        [TestMethod]
+        public void ArcID_Mult()
+        {
+            var a1 = new Arc(3);
+            var a2 = new Arc(5);
+            var a3 = new Arc(a1.Id * a2.Id);
+
+            Assert.AreEqual(15, a3.Id);
+        }
+
+        [TestMethod]
+        public void VeryLongArcID()
+        {
+            var a1 = new Arc(9223372036854775807);
+
+            Assert.AreEqual(9223372036854775807, a1.Id);
+
+            //That is Int64.MaxValue
+        }
+
+        [TestMethod]
+        public void ArcID_0()
+        {
+            var a1 = new Arc(0);
+
+            Assert.AreEqual(0, a1.Id);
+        }
+    }
+}
