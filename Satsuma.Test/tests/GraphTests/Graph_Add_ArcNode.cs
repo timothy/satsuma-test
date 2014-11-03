1155979d78747baeb64b95f81be0b8421be40f99
WesThompson <thompsonwesley8@yahoo.com>
2014-11-03 11:36:47 -0800

    commit

---
+++ Satsuma.Test/tests/GraphTests/Graph_Add_ArcNode.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Satsuma.Test.tests.GraphTests
+{
+    [TestClass]
+    public class Graph_Add_ArcNode
+    {
+        [TestMethod]
+        public void Add1Node()
+        {
+            var g = new CustomGraph();
+            g.AddNode();
+
+            Assert.AreEqual(1, g.NodeCount());
+        }
+
+        [TestMethod]
+        public void Add10Nodes()
+        {
+            var g = new CustomGraph();
+            for (int i = 0; i < 10; i++)
+            {
+                g.AddNode();
+            }
+
+            Assert.AreEqual(10, g.NodeCount());
+        }
+
+        [TestMethod]
+        public void Add1Arc()
+        {
+            var g = new CustomGraph();
+            var n1 = new Node(1);
+            var n2 = new Node(2);
+            g.AddArc(n1, n2, Directedness.Directed);
+
+            Assert.AreEqual(1, g.ArcCount());
+        }
+
+        [TestMethod]
+        public void Add10Arcs()
+        {
+            var g = new CustomGraph();
+            var n1 = new Node(1);
+            var n2 = new Node(2);
+            for (int i = 0; i < 10; i++)
+            {
+                g.AddArc(n1, n2, Directedness.Directed);
+            }
+
+            Assert.AreEqual(10, g.ArcCount());
+        }
+    }
+}
---
+++ Satsuma.Test/tests/GraphTests/Graph_Delete_ArcNode.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Satsuma.Test.tests.GraphTests
+{
+    [TestClass]
+    public class Graph_Delete_ArcNode
+    {
+        [TestMethod]
+        public void DeleteArc()
+        {
+            var g = new CustomGraph();
+            Node n1 = new Node(5);
+            Node n2 = new Node(12);
+            Arc a1 = g.AddArc(n1, n2, Directedness.Directed);
+            
+            Assert.IsTrue(g.DeleteArc(a1));
+        }
+
+        [TestMethod]
+        public void DeleteNode()
+        {
+            var g = new CustomGraph();
+            Node n1 = g.AddNode();
+
+            Assert.IsTrue(g.DeleteNode(n1));
+        }
+    }
+}
