90749d3ff3a14d8620c08ea3ef34e1efad5a374e
WesThompson <thompsonwesley8@yahoo.com>
2014-11-02 16:56:10 -0800

    commit

---
+++ Satsuma.Test/tests/GraphTests/Graph_Delete.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Satsuma.Test.tests.GraphTests
+{
+    [TestClass]
+    public class Graph_Delete
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
