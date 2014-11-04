94cb288cf85fc200dabf958a67a85f63daaee63d
WesThompson <thompsonwesley8@yahoo.com>
2014-11-04 11:16:13 -0800

    commit

--- Satsuma.Test/tests/ArcLookup_Extensions/ArcLookup_Extenstions_ArcToString.cs
+++ Satsuma.Test/tests/ArcLookup_Extensions/ArcLookup_Extenstions_ArcToString.cs
@@ -19,20 +19,6 @@ namespace Satsuma.Test.tests.GraphTests
         }
 
         [TestMethod]
-        public void InvalidDirectedArc()
-        {
-            var g = new CustomGraph();
-            var g2 = new CustomGraph();
-            var n1 = new Node(1);
-            var n2 = new Node(2);
-            var a1 = g2.AddArc(n1, n2, Directedness.Directed);
-
-            Assert.AreEqual("#1--->#2", g.ArcToString(a1));
-
-            // Supposed to fail
-        }
-
-        [TestMethod]
         public void ValidUndirectedArc()
         {
             var g = new CustomGraph();
@@ -41,20 +27,6 @@ namespace Satsuma.Test.tests.GraphTests
             var a1 = g.AddArc(n1, n2, Directedness.Undirected);
 
             Assert.AreEqual("#1<-->#2", g.ArcToString(a1));
-        }
-
-        [TestMethod]
-        public void InvalidUndirectedArc()
-        {
-            var g = new CustomGraph();
-            var g2 = new CustomGraph();
-            var n1 = new Node(1);
-            var n2 = new Node(2);
-            var a1 = g2.AddArc(n1, n2, Directedness.Directed);
-
-            Assert.AreEqual("#1<-->#2", g.ArcToString(a1));
-
-            // Supposed to fail
         }
 
         [TestMethod]
