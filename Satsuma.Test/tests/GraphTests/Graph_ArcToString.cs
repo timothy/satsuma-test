a8c9f5beb23438cfc10f12ce53d7fdcb405864c1
WesThompson <thompsonwesley8@yahoo.com>
2014-11-03 21:17:04 -0800

    commit

--- Satsuma.Test/tests/GraphTests/Graph_ArcToString.cs
+++ Satsuma.Test/tests/GraphTests/Graph_ArcToString.cs
@@ -22,9 +22,14 @@ namespace Satsuma.Test.tests.GraphTests
         public void InvalidDirectedArc()
         {
             var g = new CustomGraph();
-            var a1 = new Arc(1);
+            var g2 = new CustomGraph();
+            var n1 = new Node(1);
+            var n2 = new Node(2);
+            var a1 = g2.AddArc(n1, n2, Directedness.Directed);
 
-            Assert.AreNotEqual("#1--->#2", g.ArcToString(a1));
+            Assert.AreEqual("#1--->#2", g.ArcToString(a1));
+
+            // Supposed to fail
         }
 
         [TestMethod]
@@ -34,6 +39,68 @@ namespace Satsuma.Test.tests.GraphTests
             var n1 = new Node(1);
             var n2 = new Node(2);
             var a1 = g.AddArc(n1, n2, Directedness.Undirected);
+
+            Assert.AreEqual("#1<-->#2", g.ArcToString(a1));
+        }
+
+        [TestMethod]
+        public void InvalidUndirectedArc()
+        {
+            var g = new CustomGraph();
+            var g2 = new CustomGraph();
+            var n1 = new Node(1);
+            var n2 = new Node(2);
+            var a1 = g2.AddArc(n1, n2, Directedness.Directed);
+
+            Assert.AreEqual("#1<-->#2", g.ArcToString(a1));
+
+            // Supposed to fail
+        }
+
+        [TestMethod]
+        public void DeletedNodeDirectedU()
+        {
+            var g = new CustomGraph();
+            var n1 = new Node(1);
+            var n2 = new Node(2);
+            var a1 = g.AddArc(n1, n2, Directedness.Directed);
+            g.DeleteNode(n1);
+
+            Assert.AreEqual("#1--->#2", g.ArcToString(a1));
+        }
+
+        [TestMethod]
+        public void DeletedNodeDirectedV()
+        {
+            var g = new CustomGraph();
+            var n1 = new Node(1);
+            var n2 = new Node(2);
+            var a1 = g.AddArc(n1, n2, Directedness.Directed);
+            g.DeleteNode(n2);
+
+            Assert.AreEqual("#1--->#2", g.ArcToString(a1));
+        }
+
+        [TestMethod]
+        public void DeletedNodeUndirectedU()
+        {
+            var g = new CustomGraph();
+            var n1 = new Node(1);
+            var n2 = new Node(2);
+            var a1 = g.AddArc(n1, n2, Directedness.Undirected);
+            g.DeleteNode(n1);
+
+            Assert.AreEqual("#1<-->#2", g.ArcToString(a1));
+        }
+
+        [TestMethod]
+        public void DeletedNodeUndirectedV()
+        {
+            var g = new CustomGraph();
+            var n1 = new Node(1);
+            var n2 = new Node(2);
+            var a1 = g.AddArc(n1, n2, Directedness.Undirected);
+            g.DeleteNode(n2);
 
             Assert.AreEqual("#1<-->#2", g.ArcToString(a1));
         }
