0aa5dc7e1d406bf6028c4b626f51b9af701bc6af
WesThompson <thompsonwesley8@yahoo.com>
2014-11-03 20:17:36 -0800

    commit

--- Satsuma.Test/tests/GraphTests/Graph_ArcLookup_UV.cs
+++ Satsuma.Test/tests/GraphTests/Graph_ArcLookup_UV.cs
@@ -8,7 +8,7 @@ namespace Satsuma.Test.tests.GraphTests
     public class Graph_ArcLookup_UV
     {
         [TestMethod]
-        public void UndirectedU01()
+        public void UndirectedU()
         {
             var g = new CustomGraph();
             var n1 = new Node(0);
@@ -16,6 +16,39 @@ namespace Satsuma.Test.tests.GraphTests
             var a1 = g.AddArc(n1, n2, Directedness.Undirected);
 
             Assert.AreEqual(n1, g.U(a1));
+        }
+
+        [TestMethod]
+        public void UndirectedV()
+        {
+            var g = new CustomGraph();
+            var n1 = new Node(0);
+            var n2 = new Node(1);
+            var a1 = g.AddArc(n1, n2, Directedness.Undirected);
+
+            Assert.AreEqual(n2, g.V(a1));
+        }
+
+        [TestMethod]
+        public void DirectedU()
+        {
+            var g = new CustomGraph();
+            var n1 = new Node(0);
+            var n2 = new Node(1);
+            var a1 = g.AddArc(n1, n2, Directedness.Directed);
+
+            Assert.AreEqual(n1, g.U(a1));
+        }
+
+        [TestMethod]
+        public void DirectedV()
+        {
+            var g = new CustomGraph();
+            var n1 = new Node(0);
+            var n2 = new Node(1);
+            var a1 = g.AddArc(n1, n2, Directedness.Directed);
+
+            Assert.AreEqual(n2, g.V(a1));
         }
     }
 }
