4664d3c05a58c4b48bfa6a99dc401d6f95b61e7c
timothy <tbradford16@my.whitworth.edu>
2014-11-16 15:49:05 -0800

    Directed and Undirected CompleteBipartiteGraph red and blue node counts

--- Satsuma.Test/tests/CompleteBipartiteGraphTests.cs
+++ Satsuma.Test/tests/CompleteBipartiteGraphTests.cs
@@ -7,9 +7,33 @@ namespace Satsuma.Test.tests
     public class CompleteBipartiteGraphTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void RedNodeCountU()
         {
+            var a = new CompleteBipartiteGraph(5,5,Directedness.Undirected);
 
+            Assert.AreEqual(5, a.RedNodeCount);
+        }
+        [TestMethod]
+        public void BlueNodeCountU()
+        {
+            var a = new CompleteBipartiteGraph(5, 5, Directedness.Undirected);
+
+            Assert.AreEqual(5, a.BlueNodeCount);
+        }
+
+        [TestMethod]
+        public void RedNodeCountD()
+        {
+            var a = new CompleteBipartiteGraph(5, 5, Directedness.Directed);
+
+            Assert.AreEqual(5, a.RedNodeCount);
+        }
+        [TestMethod]
+        public void BlueNodeCountD()
+        {
+            var a = new CompleteBipartiteGraph(5, 5, Directedness.Directed);
+
+            Assert.AreEqual(5, a.BlueNodeCount);
         }
     }
 }
