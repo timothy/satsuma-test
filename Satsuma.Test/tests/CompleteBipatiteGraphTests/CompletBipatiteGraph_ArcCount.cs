5e944fef10f34143daa6169340d7197ce0e80344
timothy <tbradford16@my.whitworth.edu>
2014-11-16 17:36:14 -0800

    Added CompleteBipartiteGraphTests

---
+++ Satsuma.Test/tests/CompleteBipatiteGraphTests/CompletBipatiteGraph_ArcCount.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+//Tests By Timothy
+namespace Satsuma.Test.tests.CompleteBipatiteGraphTests
+{
+    [TestClass]
+    public class CompletBipatiteGraph_ArcCount
+    {
+        #region arcs1
+        [TestMethod]
+        public void UndirectedAll()
+        {
+            var g = new CompleteBipartiteGraph(5,5, Directedness.Undirected);
+
+            int arcs = g.ArcCount(ArcFilter.All);
+
+            Assert.AreEqual(25, arcs);
+        }
+
+        [TestMethod]
+        public void UndirectedEdge()
+        {
+            var g = new CompleteBipartiteGraph(5, 5, Directedness.Undirected);
+
+            int arcs = g.ArcCount(ArcFilter.Edge);
+
+            Assert.AreEqual(25, arcs);
+        }
+
+        [TestMethod]
+        public void DirectedAll()
+        {
+            var g = new CompleteBipartiteGraph(5, 5, Directedness.Directed);
+
+            int arcs = g.ArcCount(ArcFilter.All);
+
+            Assert.AreEqual(25, arcs);
+        }
+
+        [TestMethod]
+        public void DirectedEdge()
+        {
+            var g = new CompleteBipartiteGraph(5, 5, Directedness.Directed);
+
+            int arcs = g.ArcCount(ArcFilter.Edge);
+
+            Assert.AreEqual(0, arcs);
+        }
+
+        [TestMethod]
+        public void ArcsInvariant()
+        {
+            var g = new CompleteBipartiteGraph(5, 5, Directedness.Undirected);
+
+            int arcs1 = g.ArcCount(ArcFilter.All);
+            int arcs2 = g.ArcCount(ArcFilter.All);
+
+            Assert.AreEqual(arcs1, arcs2);
+        }
+
+        #endregion
+        #region arcs2
+        [TestMethod]
+        public void FromNodeUndirectedAll()
+        {
+            var g = new CompleteBipartiteGraph(5, 5, Directedness.Undirected);
+
+            int arcs = g.ArcCount(g.GetRedNode(0), ArcFilter.All);
+
+            Assert.AreEqual(5, arcs);
+        }
+
+        [TestMethod]
+        public void FromNodeUndirectedEdge()
+        {
+            var g = new CompleteBipartiteGraph(5, 5, Directedness.Undirected);
+
+            int arcs = g.ArcCount(g.GetRedNode(0), ArcFilter.Edge);
+
+            Assert.AreEqual(5, arcs);
+        }
+
+        [TestMethod]
+        public void FromNodeUndirectedBackward()
+        {
+            var g = new CompleteBipartiteGraph(5, 5, Directedness.Undirected);
+
+            int arcs = g.ArcCount(g.GetRedNode(0), ArcFilter.Backward);
+
+            Assert.AreEqual(5, arcs);
+        }
+
+        [TestMethod]
+        public void FromNodeUndirectedForward()
+        {
+            var g = new CompleteBipartiteGraph(5, 5, Directedness.Undirected);
+
+            int arcs = g.ArcCount(g.GetRedNode(0), ArcFilter.Forward);
+
+            Assert.AreEqual(5, arcs);
+        }
+
+        [TestMethod]
+        public void FromNodeDirectedAll()
+        {
+            var g = new CompleteBipartiteGraph(5, 5, Directedness.Directed);
+
+            int arcs = g.ArcCount(g.GetRedNode(0), ArcFilter.All);
+
+            Assert.AreEqual(5, arcs);
+        }
+
+        [TestMethod]
+        public void FromNodeDirectedEdge()
+        {
+            var g = new CompleteBipartiteGraph(5, 5, Directedness.Directed);
+
+            int arcs = g.ArcCount(g.GetRedNode(0), ArcFilter.Edge);
+
+            Assert.AreEqual(0, arcs);
+        }
+
+        [TestMethod]
+        public void FromNodeDirectedBackward()
+        {
+            var g = new CompleteBipartiteGraph(5, 5, Directedness.Directed);
+
+            int arcs = g.ArcCount(g.GetRedNode(0), ArcFilter.Backward);
+
+            Assert.AreEqual(0, arcs);
+        }
+
+        [TestMethod]
+        public void FromNodeDirectedForward()
+        {
+            var g = new CompleteBipartiteGraph(5, 5, Directedness.Directed);
+
+            int arcs = g.ArcCount(g.GetRedNode(0), ArcFilter.Forward);
+
+            Assert.AreEqual(5, arcs);
+        }
+
+        [TestMethod]
+        public void FromNodeArcsInvariant()
+        {
+            var g = new CompleteBipartiteGraph(5, 5, Directedness.Directed);
+
+            int arcs1 = g.ArcCount(g.GetRedNode(0), ArcFilter.All);
+            int arcs2 = g.ArcCount(g.GetRedNode(0), ArcFilter.All);
+
+            Assert.AreEqual(arcs1, arcs2);
+        }
+        #endregion
+        #region arcs3
+        [TestMethod]
+        public void NodeToNodeUndirectedAll()
+        {
+            var g = new CompleteBipartiteGraph(5, 5, Directedness.Undirected);
+
+            int arcs = g.ArcCount(g.GetRedNode(0), g.GetRedNode(1), ArcFilter.All);
+
+            Assert.AreEqual(0, arcs);
+        }
+
+        [TestMethod]
+        public void NodeToNodeUndirectedEdge()
+        {
+            var g = new CompleteBipartiteGraph(5, 5, Directedness.Undirected);
+
+            int arcs = g.ArcCount(g.GetRedNode(0), g.GetBlueNode(1), ArcFilter.Edge);
+
+            Assert.AreEqual(1, arcs);
+        }
+
+        [TestMethod]
+        public void NodeToNodeUndirectedBackward()
+        {
+            var g = new CompleteBipartiteGraph(5, 5, Directedness.Undirected);
+
+            int arcs = g.ArcCount(g.GetBlueNode(0), g.GetRedNode(1), ArcFilter.Backward);
+
+            Assert.AreEqual(1, arcs);
+        }
+
+        [TestMethod]
+        public void NodeToNodeUndirectedForward()
+        {
+            var g = new CompleteBipartiteGraph(5, 5, Directedness.Undirected);
+
+            int arcs = g.ArcCount(g.GetRedNode(0), g.GetRedNode(1), ArcFilter.Forward);
+
+            Assert.AreEqual(0, arcs);
+        }
+
+        [TestMethod]
+        public void NodeToNodeDirectedAll()
+        {
+            var g = new CompleteBipartiteGraph(5, 5, Directedness.Directed);
+
+            int arcs = g.ArcCount(g.GetRedNode(0), g.GetRedNode(1), ArcFilter.All);
+
+            Assert.AreEqual(0, arcs);
+        }
+
+        [TestMethod]
+        public void NodeToNodeDirectedEdge()
+        {
+            var g = new CompleteBipartiteGraph(5, 5, Directedness.Directed);
+
+            int arcs = g.ArcCount(g.GetRedNode(0), g.GetRedNode(1), ArcFilter.Edge);
+
+            Assert.AreEqual(0, arcs);
+        }
+
+        [TestMethod]
+        public void NodeToNodeDirectedBackward()
+        {
+            var g = new CompleteBipartiteGraph(5, 5, Directedness.Directed);
+
+            int arcs = g.ArcCount(g.GetRedNode(0), g.GetRedNode(1), ArcFilter.Backward);
+
+            Assert.AreEqual(0, arcs);
+        }
+
+        [TestMethod]
+        public void NodeToNodeDirectedForward()
+        {
+            var g = new CompleteBipartiteGraph(5, 5, Directedness.Directed);
+
+            int arcs = g.ArcCount(g.GetBlueNode(0), g.GetBlueNode(1), ArcFilter.Forward);
+
+            Assert.AreEqual(0, arcs);
+        }
+
+        [TestMethod]
+        public void NodeToNodeArcsInvariant()
+        {
+            var g = new CompleteBipartiteGraph(5, 5, Directedness.Undirected);
+
+            int arcs1 = g.ArcCount(g.GetRedNode(0), g.GetRedNode(1), ArcFilter.All);
+            int arcs2 = g.ArcCount(g.GetRedNode(0), g.GetRedNode(1), ArcFilter.All);
+
+            Assert.AreEqual(arcs1, arcs2);
+        }
+        #endregion
+    }
+}
--- Satsuma.Test/tests/CompleteBipatiteGraphTests/CompleteBipartiteGraphTests.cs
+++ Satsuma.Test/tests/CompleteBipatiteGraphTests/CompleteBipartiteGraphTests.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+//Tests By Timothy
 namespace Satsuma.Test.tests
 {
     [TestClass]
@@ -99,6 +99,70 @@ namespace Satsuma.Test.tests
             var c = new Node(6);
 
             Assert.AreEqual(Arc.Invalid, a.GetArc(b, c));
+        }
+        //Test case with max values for input should throw out of range exeption
+        //The following tests are testing outside the bounds
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void CompleteBipartiteGraphMaxOutOfRang()
+        {
+            var a = new CompleteBipartiteGraph(Int32.MaxValue, Int32.MaxValue, Directedness.Undirected);
+
+            Assert.AreEqual(Int32.MaxValue, a.RedNodeCount);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void CompleteBipartiteGraphMinOutOfRang()
+        {
+            var a = new CompleteBipartiteGraph(Int32.MinValue, Int32.MinValue, Directedness.Undirected);
+
+            Assert.AreEqual(Int32.MinValue, a.RedNodeCount);
+        }
+        [TestMethod]
+        public void NodeToNodeUndirectedForward()
+        {
+            var g = new CompleteBipartiteGraph(5,5, Directedness.Undirected);
+
+            int arcs = g.ArcCount(g.GetBlueNode(0), g.GetRedNode(1), ArcFilter.Forward);
+
+            Assert.AreEqual(1, arcs);
+        }
+        [TestMethod]
+        public void UndirectedAll()
+        {
+            var g = new CompleteBipartiteGraph(5, 5, Directedness.Undirected);
+
+            int arcs = g.ArcCount(ArcFilter.All);
+
+            Assert.AreEqual(25, arcs);
+        }
+        [TestMethod]
+        public void UndirectedEdge()
+        {
+            var g = new CompleteBipartiteGraph(5, 5, Directedness.Undirected);
+
+            int arcs = g.ArcCount(ArcFilter.Edge);
+
+            Assert.AreEqual(25, arcs);
+        }
+        [TestMethod]
+        public void DirectedAll()
+        {
+            var g = new CompleteBipartiteGraph(5, 5, Directedness.Directed);
+
+            int arcs = g.ArcCount(ArcFilter.All);
+
+            Assert.AreEqual(25, arcs);
+        }
+        [TestMethod]
+        public void DirectedEdge()
+        {
+            var g = new CompleteBipartiteGraph(5, 5, Directedness.Directed);
+
+            int arcs = g.ArcCount(ArcFilter.Edge);
+
+            Assert.AreEqual(0, arcs);
         }
     }
 }
